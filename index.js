0050d196bf4a23b7d324831155120db417f62f34
daneeskripter <imdanee@seznam.cz>
2022-12-30 13:42:01 +0100

    no needed function

--- index.js
+++ index.js
@@ -71,20 +71,6 @@ app.set("view-engine", "ejs")
 app.use(express.urlencoded({ extended: false}))
 app.use(limiter);
 
-function checkIfUserHaveRights(req, res) {
-  if (req.params.username == req.user.username) {
-    const file = sanitize(req.params.file)
-    if (fs.readdirSync(__dirname + config.uploadsfolder + `${sanitize(req.params.username)}/`).includes(file)) {
-      const requestedfile = fs.readFileSync(__dirname + config.uploadsfolder + `${sanitize(req.params.username)}/` + file)
-      res.send(requestedfile)
-    } else {
-      res.render(__dirname + "/views/message.ejs", {message: `<span class="material-icons">cloud_off</span>&nbsp;File ${file} not found!`,  cloudname: config.cloudname})
-    }
-  } else {
-    res.render(__dirname + "/views/message.ejs", { message: `<span class="material-icons">cloud_off</span>&nbsp;Error 401 - Unauthorized`,  cloudname: config.cloudname})
-  }
-}
-
 
 
 
