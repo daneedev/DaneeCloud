57ddc9d4a6bf6ec799f525923594e2bbd794a174
daneeskripter <imdanee@seznam.cz>
2023-01-11 18:47:52 +0100

    logging (2/2)

--- logger.js
+++ logger.js
@@ -2,16 +2,24 @@ const colors = require("colors")
 const config = require("./config.json")
 const fs = require('fs');
 
+function addZero(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+  }
+
 function logSuccess(message) {
     const date = new Date
     date.setTime(date.getTime())
     const seconds = date.getSeconds()
     const minutes = date.getMinutes()
     const hours = date.getHours()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
     if (config.logtofile) {
-        fs.appendFileSync(__dirname + "/logs/app.log", `\n[${hours}:${minutes}:${seconds}] ` + "[SUCCESS] " +  message)
+        fs.appendFileSync(__dirname + "/logs/app.log", `\n[${addZero(day)}.${addZero(month)}.${year} | ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}] ` + "[SUCCESS] " +  message)
     }
-    console.log(`[${hours}:${minutes}:${seconds}] ` + colors.green("[SUCCESS] ") +  message)
+    console.log(`[${addZero(day)}.${addZero(month)}.${year} | ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}] ` + colors.green("[SUCCESS] ") +  message)
 }
 
 function logError(message) {
@@ -20,10 +28,13 @@ function logError(message) {
     const seconds = date.getSeconds()
     const minutes = date.getMinutes()
     const hours = date.getHours()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
     if (config.logtofile) {
-        fs.appendFileSync(__dirname + "/logs/app.log", `\n[${hours}:${minutes}:${seconds}] ` + "[ERROR] " + colors.red(message))
+        fs.appendFileSync(__dirname + "/logs/app.log", `\n[${addZero(day)}.${addZero(month)}.${year} | ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}] ` + "[ERROR] " + colors.red(message))
     }
-    console.log(`[${hours}:${minutes}:${seconds}] ` + colors.red("[ERROR] ") + colors.red(message))
+    console.log(`[${addZero(day)}.${addZero(month)}.${year} | ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}] ` + colors.red("[ERROR] ") + colors.red(message))
 }
 
 function logInfo(message) {
@@ -32,10 +43,13 @@ function logInfo(message) {
     const seconds = date.getSeconds()
     const minutes = date.getMinutes()
     const hours = date.getHours()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
     if (config.logtofile) {
-        fs.appendFileSync(__dirname + "/logs/app.log", `\n[${hours}:${minutes}:${seconds}] ` + "[INFO] " + message)
+        fs.appendFileSync(__dirname + "/logs/app.log", `\n[${addZero(day)}.${addZero(month)}.${year} | ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}] ` + "[INFO] " + message)
     }
-    console.log(`[${hours}:${minutes}:${seconds}] ` + colors.blue("[INFO] ") + message)
+    console.log(`[${addZero(day)}.${addZero(month)}.${year} | ${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}] ` + colors.blue("[INFO] ") + message)
 }
 
 module.exports.logSuccess = logSuccess
