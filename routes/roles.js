19a7fb0dd4d28edca11285652975b96bf38e76b5
daneeskripter <imdanee@seznam.cz>
2023-02-10 18:11:13 +0100

    bug fix

--- routes/roles.js
+++ routes/roles.js
@@ -4,11 +4,12 @@ const {checkAuth, checkVerify, checkNotAuth} = require("../handlers/authVerify")
 const users = require("../models/users");
 const roles = require("../models/roles")
 const config = require("../config.json")
+const sanitize = require("sanitize-filename")
 
 router.get("/", checkAuth, checkVerify, async function (req, res) {
     const User = await users.findOne({username: req.user.username})
-    const role = req.query.role
-    const maxStorage = req.query.maxStorage
+    const role = sanitize(req.query.role)
+    const maxStorage = sanitize(req.query.maxStorage)
     if (User.role == "admin") {
         const updateRole = await roles.findOneAndUpdate({name: role}, {maxStorage: parseInt(maxStorage)})
         res.render(__dirname + "/../views/message.ejs", { message: `Role ${role} has been updated with maximum storage ${maxStorage} MB`,  cloudname: config.cloudname})
