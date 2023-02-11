4c46707491d2c275743d0dcb1394723468d6aaa6
daneeskripter <imdanee@seznam.cz>
2023-02-11 15:50:30 +0100

    storage limit bar added to home page

--- routes/home.js
+++ routes/home.js
@@ -3,16 +3,18 @@ const router = express.Router()
 const {checkAuth, checkVerify} = require("../handlers/authVerify")
 const users = require("../models/users");
 const config = require("../config.json")
+const roles = require("../models/roles")
 
 router.get("/", checkAuth , checkVerify, async function (req, res) {
     const user = await users.findOne({username: req.user.username})
+    const role = await roles.findOne({name: user.role})
     let isAdmin;
     if (user.role == "admin") {
       isAdmin = true
     } else {
       isAdmin = false
     }
-    res.render(__dirname + "/../views/index.ejs", {isAdmin: isAdmin, username: req.user.username, cloudname: config.cloudname} )
+    res.render(__dirname + "/../views/index.ejs", {isAdmin: isAdmin, username: req.user.username, cloudname: config.cloudname, usedStorage: user.usedStorage, maxStorage: role.maxStorage} )
   })
 
 module.exports = router
